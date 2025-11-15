5548d0d8dce078942728eb288f39e34059879c79
sohampatel03 <soham171203@gmail.com>
2025-11-15 20:26:13 +0530

    change for deployment

--- src/Utils/firebase.js
+++ src/Utils/firebase.js
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth/cordova";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAT27CRBFmyr3oVTjDPKl8WjX2s5AT_2tc",
-  authDomain: "netflix-with-chatgpt.firebaseapp.com",
-  projectId: "netflix-with-chatgpt",
-  storageBucket: "netflix-with-chatgpt.appspot.com",
-  messagingSenderId: "880879566243",
-  appId: "1:880879566243:web:27cc72adf3d60c3f8aee16",
-  measurementId: "G-XDDF0P90S8"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
